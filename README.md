fcd9890f3174f250e22cfb6d7fa46c6f669680c4
lawal bolaji <lawalabdulrasheedbolaji@gmail.com>
2021-11-13 14:28:19 +0100

    refactor awsClient.js and remove screenshot.js

--- README.md
+++
@@ -1,5 +0,0 @@
-# lawalbolaji.github.io
-
-🎆 This repo demonstrates a basic CI workflow with Github Actions.
-
-![Live image of site](https://s3.us-east-2.amazonaws.com/bonango.io-screenshots/pageShot.png)
--- awsClient.js
+++ awsClient.js
@@ -1,43 +1,67 @@
+// load node env variables
 require('dotenv').config();
-const {S3Client} = require('@aws-sdk/client-s3');
+
+// load awsC s3Client and PutBoject Command
+const { S3Client } = require('@aws-sdk/client-s3');
 const { PutObjectCommand } = require('@aws-sdk/client-s3');
-const {screenshot} = require('./screenshot');
-const fs = require('fs');
 
-const args = process.argv.slice(2); // collect terminal args
+// collect terminal args
+const args = process.argv.slice(2);
 
+// configure aws s3 client
 const REGION = process.env['AWS_DEFAULT_REGION'] || 'us-east-2';
 const AWS_ACCESS_KEY_ID = process.env['AWS_ACCESS_KEY_ID'] || args[0]; // if no access to environment varibales, get from terminal args
 const AWS_SECRET_ACCESS_KEY = process.env['AWS_SECRET_ACCESS_KEY'] || args[1];
 const fileName = 'pageShot.png';
-const url = "https://lawalbolaji.github.io";
-
-// configure aws client
 const s3Client = new S3Client({
     region: REGION,
     aws_access_key_id: AWS_ACCESS_KEY_ID,
     aws_secret_access_key: AWS_SECRET_ACCESS_KEY
 });
 
-// load file
-function loadFile(fileName){
-    try {
-        return fs.createReadStream(fileName);
-    } catch (error) {
-        console.error(error);
-    }
-};
+// load puppeteer object
+const puppeteer = require('puppeteer');
 
+// create browser instance
+async function launchBroswer(){
+    return await puppeteer.launch();
+}
 
-const params = {
-    Bucket: "bonango.io-screenshots",
-    Key: fileName,
-    Body: loadFile(fileName),
-    ContentType: "image/png",
-    ContentDisposition: "inline"
-};
+// close browser when we are done
+async function closeBroswer(browser){
+    await browser.close()
+}
 
-async function saveImage() {
+// set puppeteer params
+const URL = "https://lawalbolaji.github.io";
+const browser = launchBroswer();
+
+// screenshot function: doesnt save to file, returns binary stream of generated image
+async function screenshot(browser){
+    const page = await browser.newPage();
+
+    await page.setViewport({
+        width: 1280,
+        height: 800
+    });
+
+    await page.goto(URL);
+
+    return await page.screenshot({fullPage: true});
+}
+
+// saveImage(): dump the binary into an existing s3 bucket.
+async function saveImage(screenshot) {
+
+    // configure params for putObject Command
+    const params = {
+        Bucket: "bonango.io-screenshots",
+        Key: fileName,
+        Body: screenshot,
+        ContentType: "image/png",
+        ContentDisposition: "inline"
+    };
+
     try {
         const results = await s3Client.send(new PutObjectCommand(params));
         console.log(
@@ -49,6 +73,15 @@ async function saveImage() {
     }
 }
 
+async function run(browser){
+    browser.then(async data => {
+        const shot = await screenshot(data)
+        await closeBroswer(data);
 
-screenshot(url);
-saveImage();
+        return shot;
+
+    }).then(shot => saveImage(shot));
+}
+
+
+run(browser);
